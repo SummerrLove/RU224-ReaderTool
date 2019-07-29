d670210a4b5fbfda2808520f5152d15075442930
Ethan Lee <ethanlee.lee@gmail.com>
2019-07-29 13:12:35 +0800

    Add methods to check if string is hex and convert hex string into short array.

--- RaspbianReaderTool/src/scannel/reader/StringTool.java
+++ RaspbianReaderTool/src/scannel/reader/StringTool.java
@@ -29,4 +29,27 @@ public class StringTool {
 		
 		return hex;
 	}
+	
+	public static boolean isHexString(String str) {
+		return str.matches("[0-9a-fA-F]+");
+	}
+	
+	public static short[] toShortArray(String str) {
+		if (str==null || !isHexString(str)) {
+			MyLogger.printLog("Not hex string: "+str);
+			return null;
+		}
+		
+		if (str.length()%2 != 0) {
+			str += "0";
+		}
+		
+		short[] data = new short[str.length()/2];
+		for (int i=0; (i+1)*2<str.length(); i++) {
+			String temp = str.substring(i*2, (i+1)*2);
+			data[i] = Short.parseShort(temp, 16);
+		}
+		
+		return data;
+	}
 }
